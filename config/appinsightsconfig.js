1fd5c5aac6bc55e994c7bdb641081dbb860b4994
Xiaobin Liu <xiaol@microsoft.com>
2020-07-24 16:31:17 +0800

    add log config

--- config/appinsightsconfig.js
+++ config/appinsightsconfig.js
@@ -1,9 +1,13 @@
 {
     backends: ["./backends/appinsights"],  // [Required] The Application Insighst StatsD backend
-    aiInstrumentationKey: "your_appinsights_instrumentation_key",  // [Required] Your instrumentation key
+    aiInstrumentationKey: "",  // [Required] Your instrumentation key
     aiPrefix: "my_prefix",  // [Optional] Send only metrics with this prefix
     aiRoleName: "my_role_name",  // [Optional] Add this role name context tag to every metric
     aiRoleInstance: "my_role_instance",  // [Optional] Add this role instance context tag to every metric
     aiTrackStatsDMetrics: true,  // [Optional] Send StatsD internal metrics to Application Insights
-    debug: true
+    log: {
+        backend: "stdout",    // where to log: stdout or syslog [string, default: stdout]
+        level: ""       // log level for [node-]syslog [string, default: LOG_INFO]
+    },
+    debug: false
 }
