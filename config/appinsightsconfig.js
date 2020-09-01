12dc21793ea4c4d89460a34a99da9703995edd85
binroon <binroon@hotmail.com>
2020-09-01 15:21:25 +0800

    added configmap + environment variables used by node in runtime

--- config/appinsightsconfig.js
+++ config/appinsightsconfig.js
@@ -1,13 +1,13 @@
 {
-    backends: ["./backends/appinsights"],  // [Required] The Application Insighst StatsD backend
-    aiInstrumentationKey: "",  // [Required] Your instrumentation key
-    aiPrefix: "my_prefix",  // [Optional] Send only metrics with this prefix
-    aiRoleName: "my_role_name",  // [Optional] Add this role name context tag to every metric
-    aiRoleInstance: "my_role_instance",  // [Optional] Add this role instance context tag to every metric
+    backends: ["appinsights-statsd"],  // [Required] The Application Insighst StatsD backend
+    aiInstrumentationKey: process.env.APPLICATION_INSIGHTS_INSTRUMENTATION_KEY,  // [Required] Your instrumentation key
+    aiPrefix: "airflow",  // [Optional] Send only metrics with this prefix
+    aiRoleName: "airflow",  // [Optional] Add this role name context tag to every metric
+    aiRoleInstance: "airflow",  // [Optional] Add this role instance context tag to every metric
     aiTrackStatsDMetrics: true,  // [Optional] Send StatsD internal metrics to Application Insights
     log: {
-        backend: "stdout",    // where to log: stdout or syslog [string, default: stdout]
+        backend: "syslog",    // where to log: stdout or syslog [string, default: stdout]
         level: ""       // log level for [node-]syslog [string, default: LOG_INFO]
     },
-    debug: false
+    debug: true
 }
